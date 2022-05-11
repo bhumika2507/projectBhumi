58b966a443d24589dfce7d59b6cdebf987b28f18
bhumika2507 <bhumika.suru@webcluesinfotech.com>
2022-05-11 10:55:06 +0530

    initiale commit

---
+++ index.js
@@ -0,0 +1,63 @@
+ //let num = 4
+//console.log(num);
+
+//num = 9
+//console.log(num);
+
+//let user_name = 'Bhumika'
+//console.log(user_name)
+
+//let radious = 5
+//let pi = 3.14
+//let area
+
+//area = pi * radious * radious
+
+//console.log(area)
+
+//let num = 8888
+//console.log(num * 2);
+
+//let num = 88
+//console.log( typeof num);
+
+//let firsname = "bhumi"
+//let lastname = "suru"
+
+//let user = "Bhu\tmika \nSu\buru"
+//console.log(user)
+
+
+//let bool= 5 > 6
+//console.log( typeof bool);
+
+//let user 
+//console.log(typeof user);
+
+//let num = Number("12345");
+
+//console.log(num,typeof(num));
+
+//let x;
+
+//console.log(x,typeof(x));
+//x=8;
+//console.log(x,typeof(x));
+//x= x + ""
+//console.log(x,typeof(x));
+//x = x - 2;
+//console.log(x,typeof(x));
+//x = !x
+//console.log(x,typeof(x));
+
+//console.log(Boolean(7));
+//console.log(Boolean("bhumi"));
+
+//let num=4;
+//num=num + 2;
+//num += 2
+//num--;
+
+//console.log(num);
+
+
