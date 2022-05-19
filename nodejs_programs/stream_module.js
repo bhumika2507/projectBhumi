47144228edb59ae1f54bdb8d2cd16f24aed2f5e5
bhumika2507 <bhumika.suru@webcluesinfotech.com>
2022-05-19 18:52:07 +0530

    programs

--- nodejs_programs/stream_module.js
+++ nodejs_programs/stream_module.js
@@ -1,16 +1,17 @@
 const fs =require("fs");
 const http=require("http");
 
-const server = http.createServer();
+// const server = http.createServer();
 
-server.on("request",(req,res) =>
-{
-   let fs =require('fs');
-   fs.readFile("input.txt",function(err,data)
-   {
-       if(err) return console.error(err);
-       res.end(data.toString());
-   }); 
-})
+// server.on("request",(req,res) =>
+// {
+//    let fs =require('fs');
+//    fs.readFile("input.txt",function(err,data)
+//    {
+//        if(err) return console.error(err);
+//        res.end(data.toString());
+//    }); 
 
-server.listen(8000,"127.0.0.1");
+
+// server.listen(8000,"127.0.0.1");
+
