3cf43c6aa854cfb61c0c5ab7e5747f6dea241b1d
bhumika2507 <bhumika.suru@webcluesinfotech.com>
2022-05-27 14:36:02 +0530

    localstorage

--- crud-operation.js
+++ crud-operation.js
@@ -1,7 +1,5 @@
-
-    
-   
 let selectedRow = null
+var taskWrapper = document.getElementById("task_wrapper");
 
 const onFormSubmit = () =>
  {
@@ -16,9 +14,25 @@ const onFormSubmit = () =>
     }
 }
 
+let  data1= [];
+
+ const onloadData = () =>
+ {
+
+    const data =JSON.parse(localStorage.getItem("emp"))
+    if(data.length>0)
+    {
+        for(i=0;i<data.length;i++)
+        {
+            insertNewRecord(data[i]);
+        }
+    }
+ }
+
 const readFormData = () =>
 {
     let formData = {};
+    formData["id"] = document.getElementById("id").value;
     formData["fullName"] = document.getElementById("fullName").value;
     formData["email"] = document.getElementById("email").value;
     formData["salary"] = document.getElementById("salary").value;
@@ -28,69 +42,98 @@ const readFormData = () =>
 
 const insertNewRecord = (data) =>  
 {
-     const data1 =JSON.parse(localStorage.getItem("emp")) || [];
-
-    
-
     let table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
     let newRow = table.insertRow(table.length);
+
     cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.id;
+    cell1 = newRow.insertCell(1);
     cell1.innerHTML = data.fullName;
-    cell2 = newRow.insertCell(1);
+    cell2 = newRow.insertCell(2);
     cell2.innerHTML = data.email;
-    cell3 = newRow.insertCell(2);
+    cell3 = newRow.insertCell(3);
     cell3.innerHTML = data.salary;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;
     cell4 = newRow.insertCell(4);
+    cell4.innerHTML = data.city;
+    cell4 = newRow.insertCell(5);
     cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
                        <a onClick="onDelete(this)">Delete</a>`;
     data1.push(data);
     localStorage.setItem("emp",JSON.stringify(data1));
-    return data1;
+   
 }
-
 const resetForm = () =>
 {
+    document.getElementById("id").value = "";
     document.getElementById("fullName").value = "";
     document.getElementById("email").value = "";
     document.getElementById("salary").value = "";
     document.getElementById("city").value = "";
     selectedRow = null;
+
+    
 }
 
 const onEdit = (td) =>
 {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("email").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+    
+    document.getElementById("id").disabled=true;
+    document.getElementById("id").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("fullName").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("salary").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("city").value = selectedRow.cells[4].innerHTML;
+   
 }
 const updateRecord = (formData)  => 
 {
-    let existing = localStorage.getItem('myFavoriteSandwich');
+   
 
-    selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.email;
-    selectedRow.cells[2].innerHTML = formData.salary;
-    selectedRow.cells[3].innerHTML = formData.city;
+   
+     let myid=selectedRow.cells[0].innerHTML = formData.id;
+    selectedRow.cells[1].innerHTML = formData.fullName;
+    selectedRow.cells[2].innerHTML = formData.email;
+    selectedRow.cells[3].innerHTML = formData.salary;
+    selectedRow.cells[4].innerHTML = formData.city;
+    
+        
+        let editing=JSON.parse(localStorage.getItem("emp"));
+        
+        
 
-      existing = existing ? existing.split(',') : [];
-      existing.push('tuna');
-      localStorage.setItem('myFavoriteSandwich', existing.toString());
+        objIndex = editing.findIndex((obj => obj.id == myid));
+    
+    editing[objIndex] = formData
+    
+    localStorage.setItem("emp",JSON.stringify(editing));
+    
+        
+    
 
 }
 
 const onDelete = (td) => 
  {
-    if (confirm('Are you sure to delete this record ?')) {
+    if (confirm('Are you sure to delete this record ?')) 
+    {
         row = td.parentElement.parentElement;
-        document.getElementById("employeeList").deleteRow(row.rowIndex);
-        resetForm();
+        const del= document.getElementById("employeeList").deleteRow(row.rowIndex);
+        selectedRow = td.parentElement.parentElement;
+        const deleteRecordIndex = selectedRow.cells[0].innerHTML
+        let deleting = JSON.parse(localStorage.getItem('emp'));
+        objIndex = deleting.findIndex((obj => obj.id == deleteRecordIndex));
+        deleting.splice(objIndex,1);
+        localStorage.setItem('emp',JSON.stringify(deleting));
+            resetForm();
+        }
     }
-    localStorage.clear();
-}
+    
+
+    
+    
+
+
 const validate =() => 
 {
     isValid = true;
@@ -104,24 +147,4 @@ const validate =() =>
     }
     return isValid;
 }
-
-// const myFunction = () =>
-//  {
-//     let  input, filter, table, tr, td, i, txtValue;
-//     input = document.getElementById("myInput");
-//     filter = input.value.toUpperCase();
-//     table = document.getElementById("employeeList");
-//     tr = table.getElementsByTagName("tr");
-//     for (i = 0; i < tr.length; i++) {
-//       td = tr[i].getElementsByTagName("td")[0];
-//       if (td) {
-//         txtValue = td.textContent || td.innerText;
-//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//           tr[i].style.display = "";
-//         } else {
-//           tr[i].style.display = "none";
-//         }
-//       }       
-//     }
-//   }
 
